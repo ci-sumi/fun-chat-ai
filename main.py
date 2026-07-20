45baadd0d2c2cdc487c4bcd94f725dd3722fd6a9
ci-sumi <tharayilsurendransumi@gmail.com>
2026-07-20 12:56:07 +0100

    Integrate Groq's AI chat model and load environment variables in main.py

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+import os
+from dotenv import load_dotenv
+# python talk to Groq's AI chat models.
+from langchain_groq import ChatGroq
+
 from flask import Flask
 
-
+load_dotenv()
+apikey=os.getenv('GROQ_API_KEY')
+print(apikey)
+model="llama-3.3-70b-versatile"
+deepseek =ChatGroq(api_key=apikey,model=model)
+print(deepseek.invoke("Hello, Tomi Tomi!"))
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
